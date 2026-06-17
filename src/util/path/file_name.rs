149c5792d3da24103222090d7e4a4bbfb4d511b7
gifnksm <makoto.nksm+github@gmail.com>
2026-06-17 19:27:58 +0900

    Serialize UTF-8 file names as strings

--- src/util/path/file_name.rs
+++ src/util/path/file_name.rs
@@ -4,10 +4,9 @@ use std::{
     path::{Path, PathBuf},
 };
 
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize, Serialize, de};
 
-#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Serialize)]
-#[serde(transparent)]
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub(crate) struct FileName(OsString);
 
 impl FileName {
@@ -35,13 +34,36 @@ impl FileName {
     }
 }
 
+impl Serialize for FileName {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        if let Some(s) = self.0.to_str() {
+            str::serialize(s, serializer)
+        } else {
+            OsStr::serialize(&self.0, serializer)
+        }
+    }
+}
+
 impl<'de> Deserialize<'de> for FileName {
     fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
     where
         D: serde::Deserializer<'de>,
     {
-        let s = OsString::deserialize(deserializer)?;
-        FileName::new(s).ok_or_else(|| serde::de::Error::custom("invalid file name"))
+        #[derive(Deserialize)]
+        #[serde(untagged)]
+        enum Repr {
+            String(String),
+            OsString(OsString),
+        }
+
+        let name = match Repr::deserialize(deserializer)? {
+            Repr::String(v) => v.into(),
+            Repr::OsString(v) => v,
+        };
+        FileName::new(name).ok_or_else(|| de::Error::custom("invalid file name"))
     }
 }
 
@@ -102,7 +124,19 @@ impl_partial_eq_for_file_name!(
 
 #[cfg(test)]
 mod tests {
+    use std::{ffi::OsString, os::windows::ffi::OsStringExt as _};
+
     use super::*;
+
+    fn make_non_utf8_os_string() -> OsString {
+        OsString::from_wide(&[
+            0xD800,
+            u16::from(b'.'),
+            u16::from(b't'),
+            u16::from(b't'),
+            u16::from(b'f'),
+        ])
+    }
 
     #[test]
     fn file_name_new_accepts_plain_file_name() {
@@ -122,5 +156,45 @@ mod tests {
         ] {
             assert!(FileName::new(file_name).is_none());
         }
+    }
+
+    #[test]
+    fn serialize_uses_json_string_for_utf8_file_name() {
+        let file_name = FileName::new("example-font.ttf").unwrap();
+
+        assert_eq!(
+            serde_json::to_string(&file_name).unwrap(),
+            r#""example-font.ttf""#
+        );
+    }
+
+    #[test]
+    fn deserialize_accepts_json_string_for_utf8_file_name() {
+        let file_name: FileName = serde_json::from_str(r#""example-font.ttf""#).unwrap();
+
+        assert_eq!(file_name, FileName::new("example-font.ttf").unwrap());
+    }
+
+    #[test]
+    fn deserialize_accepts_platform_os_string_representation() {
+        let value = serde_json::to_value(OsString::from("example-font.ttf")).unwrap();
+        let file_name: FileName = serde_json::from_value(value).unwrap();
+
+        assert_eq!(file_name, FileName::new("example-font.ttf").unwrap());
+    }
+
+    #[test]
+    fn non_utf8_file_name_roundtrips_via_json() {
+        let os_string = make_non_utf8_os_string();
+        let file_name = FileName::new(&os_string).unwrap();
+
+        assert_eq!(
+            serde_json::to_value(&file_name).unwrap(),
+            serde_json::to_value(&os_string).unwrap(),
+        );
+        assert_eq!(
+            serde_json::from_value::<FileName>(serde_json::to_value(&file_name).unwrap()).unwrap(),
+            file_name,
+        );
     }
 }
